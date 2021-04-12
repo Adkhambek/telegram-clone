151ff1e0432c58f0bfa1beddabf9fe84e914a76c
Adham <muzaffarov.adham@gmail.com>
2021-04-12 11:17:26 +0500

    store data

---
+++ assets/js/store.js
@@ -0,0 +1,190 @@
+const data = [
+    {
+        id: 1,
+        name: "Adham Muzaffarov",
+        phone: "+998 99 8000334",
+        avatar: "./images/placeholder/1.jpg",
+        username: "Heaven_8",
+        bio: ""
+    },
+    {
+        id: 2,
+        name: "Abdulboriy",
+        phone: "+998 99 8612931",
+        avatar: "./images/placeholder/4.jpg",
+        username: "Straight_path0599",
+        bio: "",
+        messages: [
+            {
+                body: "Assalomu alekum",
+                date: "12:15 AM",
+                isMine: false
+            },
+            {
+                body: "Valekum Assalom",
+                date: "12:16 AM",
+                isMine: true  
+            }
+        ]
+    },
+    {
+        id: 3,
+        name: "Bobir aka",
+        phone: "+998 93 6667626",
+        avatar: "./images/placeholder/3.jpg",
+        username: "MrBobyr",
+        bio: "Мммм дааа зачем",
+        messages: [
+            {
+                body: "Assalomu alekum",
+                date: "12:15 AM",
+                isMine: false
+            },
+            {
+                body: "Valekum Assalom",
+                date: "12:16 AM",
+                isMine: true  
+            }
+        ]
+    },
+    {
+        id: 4,
+        name: "Abdulloh English",
+        phone: "+998 97 7413933",
+        avatar: "./images/placeholder/5.jpg",
+        username: "Ismoilov_Abdulloh",
+        bio: "Beshikdan to qabrgacha ilm izla",
+        messages: [
+            {
+                body: "Assalomu alekum",
+                date: "12:15 AM",
+                isMine: false
+            },
+            {
+                body: "Valekum Assalom",
+                date: "12:16 AM",
+                isMine: true  
+            }
+        ]
+    },
+    {
+        id: 5,
+        name: "Alisher",
+        phone: "+998 94 6611082",
+        avatar: "./images/placeholder/7.jpg",
+        username: "LazyPerfectionist",
+        bio: "Receive the message to realise the messenger",
+        messages: [
+            {
+                body: "Assalomu alekum",
+                date: "12:15 AM",
+                isMine: false
+            },
+            {
+                body: "Valekum Assalom",
+                date: "12:16 AM",
+                isMine: true  
+            }
+        ]
+    },
+    {
+        id: 6,
+        name: "Fotima",
+        phone: "+998 93 1600663",
+        avatar: "./images/placeholder/6.jpg",
+        username: "",
+        bio: "",
+        messages: [
+            {
+                body: "Assalomu alekum",
+                date: "12:15 AM",
+                isMine: false
+            },
+            {
+                body: "Valekum Assalom",
+                date: "12:16 AM",
+                isMine: true  
+            }
+        ]
+    },
+    {
+        id: 7,
+        name: "Muhammad",
+        phone: "+998 90 1270111",
+        avatar: "./images/placeholder/8.jpg",
+        username: "givmeaproofthatiexist",
+        bio: "Give me proof that i exist",
+        messages: [
+            {
+                body: "Assalomu alekum",
+                date: "12:15 AM",
+                isMine: false
+            },
+            {
+                body: "Valekum Assalom",
+                date: "12:16 AM",
+                isMine: true  
+            }
+        ]
+    },
+    {
+        id: 8,
+        name: "Abdulazizkhan",
+        phone: "+998 99 8835336",
+        avatar: "./images/placeholder/9.jpg",
+        username: "Richkhan",
+        bio: "ما أصابك لم يكن ليخطئُك وما يخطئُك لم يكن ليُصيبك",
+        messages: [
+            {
+                body: "Assalomu alekum",
+                date: "12:15 AM",
+                isMine: false
+            },
+            {
+                body: "Valekum Assalom",
+                date: "12:16 AM",
+                isMine: true  
+            }
+        ]
+    },
+    {
+        id: 9,
+        name: "Sarvar",
+        phone: "Hidden",
+        avatar: "./images/placeholder/10.jpg",
+        username: "Richkhan",
+        bio: "",
+        messages: [
+            {
+                body: "Assalomu alekum",
+                date: "12:15 AM",
+                isMine: false
+            },
+            {
+                body: "Valekum Assalom",
+                date: "12:16 AM",
+                isMine: true  
+            }
+        ]
+    },
+    {
+        id: 10,
+        name: "Bahtiyor",
+        phone: "+998 99 8970047",
+        avatar: "./images/placeholder/11.jpg",
+        username: "",
+        bio: "",
+        messages: [
+            {
+                body: "Assalomu alekum",
+                date: "12:15 AM",
+                isMine: false
+            },
+            {
+                body: "Valekum Assalom",
+                date: "12:16 AM",
+                isMine: true  
+            }
+        ]
+    }
+]
