781648059b2a4497dcf0bd9da19bcd56d494dd99
Adham <muzaffarov.adham@gmail.com>
2021-04-14 07:22:04 +0500

    localstorage

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,31 @@
 const menuBtn = document.querySelector('.dialog__menu-btn')
 const menu = document.querySelector('.menu')
 const overlay = document.querySelector('.overlay')
+const search = document.querySelector('.dialog__search-bar')
+const chatSearch = document.querySelector('.chat__top-btn.search')
+const menuBar = document.querySelector('.chat__top-btn.menu-bar')
+const dropdown = menuBar.querySelector('.dropdown')
+const profile = document.querySelector('.profile')
+const chatDetail = document.querySelector('.chat__detail')
+const closeBtn = document.querySelector('.close-btn')
+const textarea = document.querySelector('.chat__message-area')
+const recorder = document.querySelector('.chat__audio-recorder')
+const send = document.querySelector('.submit')
+const chatRight = document.querySelector('.chat__right')
+const chatBody = document.querySelector('.chat__body')
+const chatFooter = document.querySelector('.chat__footer')
+const selectChat = document.querySelector('.select-chat')
+const dialogItem = document.querySelectorAll('.dialog__item')
+const textArea = document.querySelector('.chat__message-area')
+const openProfile = function(){
+    profile.classList.remove('hidden')
+    overlay.classList.remove('hidden') 
+}
+const closeProfile = function(){
+    profile.classList.add('hidden')
+    overlay.classList.add('hidden') 
+} 
+
 const openMenu = function(){
     menu.classList.add('show-menu')
     overlay.classList.remove('hidden') 
@@ -20,8 +45,7 @@ document.addEventListener('keydown', function (e) {
   });
 
 // SearchBar: 
-const search = document.querySelector('.dialog__search-bar')
-const chatSearch = document.querySelector('.chat__top-btn.search')
+
 
 search.addEventListener('click', ()=>{
     search.classList.add('outline')
@@ -37,8 +61,7 @@ search.addEventListener('focusout', ()=>{
 }) 
 
 //Dropdown: 
-const menuBar = document.querySelector('.chat__top-btn.menu-bar')
-const dropdown = menuBar.querySelector('.dropdown')
+
 
 menuBar.addEventListener('click', (e)=>{
     dropdown.classList.remove('hidden');
@@ -51,21 +74,13 @@ window.document.addEventListener('click',()=>{
 
 //Profile Modal: 
 
-const profile = document.querySelector('.profile')
-const chatDetail = document.querySelector('.chat__detail')
-const closeBtn = document.querySelector('.close-btn')
-const openProfile = function(){
-    profile.classList.remove('hidden')
-    overlay.classList.remove('hidden') 
-}
-const closeProfile = function(){
-    profile.classList.add('hidden')
-    overlay.classList.add('hidden') 
-} 
 
 chatDetail.addEventListener('click', openProfile)
+
 overlay.addEventListener('click', closeProfile)
+
 closeBtn.addEventListener('click', closeProfile)
+
 document.addEventListener('keydown', function (e) {
     if (e.key === 'Escape' && !chatDetail.classList.contains('hidden')) {
       closeProfile()
@@ -74,13 +89,10 @@ document.addEventListener('keydown', function (e) {
 
   //Send Form:
 
-  const textarea = document.querySelector('.chat__message-area')
-  const recorder = document.querySelector('.chat__audio-recorder')
-  const send = document.querySelector('.submit')
+  
   
   textarea.addEventListener("input", ()=>{
-      
-  textarea.setAttribute("style", "height:" + (textarea.scrollHeight) + "px;");
+      textarea.setAttribute("style", "height:" + (textarea.scrollHeight) + "px;");
       recorder.classList.add('hidden')
       send.classList.remove('hidden')
     if (!textarea.value.length) {
@@ -93,35 +105,15 @@ document.addEventListener('keydown', function (e) {
     } 
     }, false);
 
-// Selecting Chat: 
-const chatRight = document.querySelector('.chat__right')
-const chatBody = document.querySelector('.chat__body')
-const chatFooter = document.querySelector('.chat__footer')
-const chatList = document.querySelector('.chat__list')
-const selectChat = document.querySelector('.select-chat')
-const dialogItem = document.querySelectorAll('.dialog__item')
-const textArea = document.querySelector('.chat__message-area')
 
 
-dialogItem.forEach(value=>{
-  
-    value.addEventListener('click', ()=>{
-        for (const item of dialogItem) {
-            if (item.classList.contains('active')) {
-                 item.classList.remove('active') 
-            }
-        }
-        value.classList.add('active')
-        chatDetail.classList.remove('hidden')
-        chatRight.classList.remove('hidden')
-        chatFooter.classList.remove('hidden')
-        chatList.classList.remove('hidden')
-        selectChat.classList.add('hidden')
-        chatBody.classList.add('height-auto') 
-        
-        })
-      
-}) 
+
+
+
+
+
+
+
 
 
 
--- assets/js/store.js
+++ assets/js/store.js
@@ -1,21 +1,19 @@
-const data = [
-    {
+let dataArr = [{
         id: 1,
         name: "Adham Muzaffarov",
         phone: "+998 99 8000334",
         avatar: "1.jpg",
         username: "Heaven_8",
         bio: "",
-        messages: [
-            {
-                body: "Assalomu alekum",
+        messages: [{
+                body: "Assalomu alekum.qalesan o'rtoq yaxshimisan axvollarin yaxshimi axvollaarin yaxshimi",
                 date: "12:15 AM",
                 isMine: false
             },
             {
                 body: "Valekum Assalom",
                 date: "12:16 AM",
-                isMine: true  
+                isMine: true
             }
         ]
     },
@@ -26,8 +24,7 @@ const data = [
         avatar: "4.jpg",
         username: "Straight_path0599",
         bio: "",
-        messages: [
-            {
+        messages: [{
                 body: "Assalomu alekum",
                 date: "12:15 AM",
                 isMine: false
@@ -35,7 +32,7 @@ const data = [
             {
                 body: "Valekum Assalom",
                 date: "12:16 AM",
-                isMine: true  
+                isMine: true
             }
         ]
     },
@@ -46,8 +43,7 @@ const data = [
         avatar: "3.jpg",
         username: "MrBobyr",
         bio: "Мммм дааа зачем",
-        messages: [
-            {
+        messages: [{
                 body: "Assalomu alekum",
                 date: "12:15 AM",
                 isMine: false
@@ -55,7 +51,7 @@ const data = [
             {
                 body: "Valekum Assalom",
                 date: "12:16 AM",
-                isMine: true  
+                isMine: true
             }
         ]
     },
@@ -66,8 +62,7 @@ const data = [
         avatar: "5.jpg",
         username: "Ismoilov_Abdulloh",
         bio: "Beshikdan to qabrgacha ilm izla",
-        messages: [
-            {
+        messages: [{
                 body: "Assalomu alekum",
                 date: "12:15 AM",
                 isMine: false
@@ -75,7 +70,7 @@ const data = [
             {
                 body: "Valekum Assalom",
                 date: "12:16 AM",
-                isMine: true  
+                isMine: true
             }
         ]
     },
@@ -86,8 +81,7 @@ const data = [
         avatar: "7.jpg",
         username: "LazyPerfectionist",
         bio: "Receive the message to realise the messenger",
-        messages: [
-            {
+        messages: [{
                 body: "Assalomu alekum",
                 date: "12:15 AM",
                 isMine: false
@@ -95,7 +89,7 @@ const data = [
             {
                 body: "Valekum Assalom",
                 date: "12:16 AM",
-                isMine: true  
+                isMine: true
             }
         ]
     },
@@ -106,8 +100,7 @@ const data = [
         avatar: "6.jpg",
         username: "",
         bio: "",
-        messages: [
-            {
+        messages: [{
                 body: "Assalomu alekum",
                 date: "12:15 AM",
                 isMine: false
@@ -115,7 +108,7 @@ const data = [
             {
                 body: "Valekum Assalom",
                 date: "12:16 AM",
-                isMine: true  
+                isMine: true
             }
         ]
     },
@@ -126,8 +119,7 @@ const data = [
         avatar: "8.jpg",
         username: "givmeaproofthatiexist",
         bio: "Give me proof that i exist",
-        messages: [
-            {
+        messages: [{
                 body: "Assalomu alekum",
                 date: "12:15 AM",
                 isMine: false
@@ -135,7 +127,7 @@ const data = [
             {
                 body: "Valekum Assalom",
                 date: "12:16 AM",
-                isMine: true  
+                isMine: true
             }
         ]
     },
@@ -146,8 +138,7 @@ const data = [
         avatar: "9.jpg",
         username: "Richkhan",
         bio: "ما أصابك لم يكن ليخطئُك وما يخطئُك لم يكن ليُصيبك",
-        messages: [
-            {
+        messages: [{
                 body: "Assalomu alekum",
                 date: "12:15 AM",
                 isMine: false
@@ -155,12 +146,12 @@ const data = [
             {
                 body: "Valekum Assalom",
                 date: "12:16 AM",
-                isMine: true  
+                isMine: true
             },
             {
                 body: "Qalesan",
                 date: "12:16 AM",
-                isMine: false  
+                isMine: false
             }
         ]
     },
@@ -171,8 +162,7 @@ const data = [
         avatar: "10.jpg",
         username: "Richkhan",
         bio: "",
-        messages: [
-            {
+        messages: [{
                 body: "Assalomu alekum",
                 date: "12:15 AM",
                 isMine: false
@@ -180,7 +170,7 @@ const data = [
             {
                 body: "Valekum Assalom",
                 date: "12:16 AM",
-                isMine: true  
+                isMine: true
             }
         ]
     },
@@ -191,8 +181,7 @@ const data = [
         avatar: "11.jpg",
         username: "",
         bio: "",
-        messages: [
-            {
+        messages: [{
                 body: "Assalomu alekum",
                 date: "12:15 AM",
                 isMine: false
@@ -200,53 +189,99 @@ const data = [
             {
                 body: "Valekum Assalom",
                 date: "12:16 AM",
-                isMine: true  
+                isMine: true
             }
         ]
     }
-] 
+]
+
+const dialogList = document.querySelector(".dialog__list")
+const chatList = document.querySelector('.chat__list')
+const searchForm = document.querySelector('.dialog__search-form')
+const searchInput = document.querySelector('.dialog__search-bar')
+const chatForm = document.querySelector('.chat__form')
+const formInput = document.querySelector('.chat__message-area')
+let currentChatId = 0
+let inputValue = ""
 
 
+// LocalStorage 
 
-renderUser(data) 
+let data = localStorage.getItem('users')
+let usersData = JSON.parse(data)
 
-function renderUser(data){
-    for(let item of data){
-       let lastMessage = item.messages[item.messages.length-1]
-       newDealogElement(item.avatar, item.name, lastMessage.isMine, lastMessage.body, lastMessage.date)  
-      
-    }
+if (!usersData) {
+    localStorage.setItem("users", JSON.stringify(dataArr))
 } 
 
 
-function newDealogElement(image, username, isMine, lastMessage, time){
-    let dialogList = document.querySelector(".dialog__list")
+chatForm.addEventListener('submit', e=>{
+    e.preventDefault()
+    
+    let selectedID = usersData.find(user => user.id == currentChatId)
+    let selectedIndex = usersData.findIndex(user => user.id == currentChatId)
+    selectedID.messages.push(
+        {
+            body: formInput.value,
+            date: "12:12",
+            isMine: true 
+        }
+    )
+   
+    usersData[selectedIndex] = selectedID
+    localStorage.setItem("users", JSON.stringify(usersData)) 
+    
+    
+    
+  chatList.textContent = "";
+  for (let message of selectedID.messages) {
+    newChatListItem(chatList, message.body, message.date, message.isMine)   
+  }   
+  
+  e.target.reset();
+})
+
+renderUser(usersData) 
+
+
+function renderUser(data) {
+    for (let item of data) {
+        let lastMessage = item.messages[item.messages.length - 1]
+        newDealogElement(item.avatar, item.name, lastMessage.isMine, lastMessage.body, lastMessage.date, item)
+    }
+}
+
+
+function newDealogElement(image, username, isMine, lastMessage, time, item) {
+
     const dialogItem = document.createElement("li")
-    dialogItem.setAttribute("class","dialog__item")
+    dialogItem.setAttribute("class", "dialog__item")
+    selectUserList(dialogItem, item)
+
     const dialogAvatar = document.createElement("img")
-    dialogAvatar.setAttribute("src","./assets/images/placeholder/"+image)
-    dialogAvatar.setAttribute("alt","dialog avatar")
-    dialogAvatar.setAttribute("class","dialog__avatar")
-    const detailBox =  document.createElement("div")
-    detailBox.setAttribute("class","dialog__detail")
+    dialogAvatar.setAttribute("src", "./assets/images/placeholder/" + image)
+    dialogAvatar.setAttribute("alt", "dialog avatar")
+    dialogAvatar.setAttribute("class", "dialog__avatar")
+    const detailBox = document.createElement("div")
+    detailBox.setAttribute("class", "dialog__detail")
     const userName = document.createElement("h2")
-    userName.setAttribute("class","dialog__user-name")
+    userName.setAttribute("class", "dialog__user-name")
     userName.textContent = username
     const dialogMessage = document.createElement("p")
-    dialogMessage.setAttribute("class","dialog__message")
+    dialogMessage.setAttribute("class", "dialog__message")
     dialogMessage.textContent = lastMessage
     const sender = document.createElement("span")
-    sender.setAttribute("class", "sender")  
+    sender.setAttribute("class", "sender")
     sender.textContent = "You:"
     const dialogRight = document.createElement("div")
-    dialogRight.setAttribute("class","dialog__right")
+    dialogRight.setAttribute("class", "dialog__right")
     const faCheck = document.createElement("span")
-    faCheck.setAttribute("class","fas fa-check")
+    faCheck.setAttribute("class", "fas fa-check")
     const dialogTime = document.createElement("p")
-    dialogTime.setAttribute("class","dialog__time")
+    dialogTime.setAttribute("class", "dialog__time")
     dialogTime.textContent = time
 
-    if(isMine){
+    if (isMine) {
         dialogRight.appendChild(faCheck)
         dialogMessage.prepend(sender)
     }
@@ -258,6 +293,87 @@ function newDealogElement(image, username, isMine, lastMessage, time){
     dialogItem.appendChild(dialogRight)
     dialogList.appendChild(dialogItem)
 
-
 }
+
+
+function newChatListItem(parent, message, time, isMine) {
+    const chatItem = document.createElement('li')
+    chatItem.setAttribute('class', 'chat__content')
+    const divBox = document.createElement('div')
+    divBox.setAttribute('class', 'chat__box')
+    const chatText = document.createElement('p')
+    chatText.setAttribute('class', 'chat__text')
+    chatText.textContent = message
+    const boxRight = document.createElement('div')
+    boxRight.setAttribute('class', 'chat__box-right')
+    const mesgtime = document.createElement('time')
+    mesgtime.setAttribute('class', 'time')
+    mesgtime.textContent = time
+    const fasCheck = document.createElement('span')
+    fasCheck.setAttribute('class', 'fas fa-check')
+
+    if (isMine) {
+        chatItem.classList.add("me")
+        boxRight.appendChild(fasCheck)
+    }
+    chatItem.appendChild(divBox)
+    divBox.appendChild(chatText)
+    divBox.appendChild(boxRight)
+    boxRight.appendChild(mesgtime)
+    parent.appendChild(chatItem)
+}
+
+
+function selectUserList(listItem, item) {
+
+    listItem.addEventListener('click', () => {
+        const allDialogItem = document.querySelectorAll('.dialog__item')
+        currentChatId = item.id
+        console.log(currentChatId);
+        let selectedObject = usersData.find(user => user.id == currentChatId)
+        
+        chatList.textContent = ""
+        for (let message of selectedObject.messages) {
+            newChatListItem(chatList, message.body, message.date, message.isMine)   
+        }
+
+
+        for (let item of allDialogItem) {
+            if (item.classList.contains('active')) {
+                item.classList.remove('active')
+            }
+        }
+        listItem.classList.add('active')
+        chatDetail.classList.remove('hidden')
+        chatRight.classList.remove('hidden')
+        chatFooter.classList.remove('hidden')
+        chatList.classList.remove('hidden')
+        selectChat.classList.add('hidden')
+        chatBody.classList.add('height-auto')
+        formInput.value = ""
+
+
+    })
+
+    
+}
+
+
+searchForm.addEventListener('submit', e => {
+    e.preventDefault()
+})
+
+
+searchInput.addEventListener('input', e => {
+    dialogList.textContent = ""
+    if(!inputValue){
+        renderUser(usersData)
+    }
+    inputValue = e.target.value;
+    
+    let searchResult = usersData.filter(value=>{
+        return value.name.toLowerCase().includes(inputValue)
+    })
+    renderUser(searchResult)
+})
 
